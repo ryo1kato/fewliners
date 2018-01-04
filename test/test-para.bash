5009f332344d6e4a9d79cceef6ad6e6f9380324e
Ryoichi KATO <ryo1kato@gmail.com>
2018-01-03 22:32:19 -0800

    para: support for GNU parallel like {.}, {/}, {//}, {/.}

--- test/test-para.bash
+++ test/test-para.bash
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+errors=0
+PARA=./para
+
 LOG () {
     echo
     echo "* $*"
+}
+
+OK () {
+    echo OK
+}
+
+FAIL () {
+    echo FAIL "$@" >2
+    let errors++
+}
+
+ASSERT_EQ () {
+    if [ "$1" == "$2" ]
+    then
+        OK
+    else
+        FAIL "'$1' != '$2'"
+    fi
 }
 
 check_outputs () {
@@ -12,10 +33,9 @@ check_outputs () {
         md5here=$(printf "%s" "$rand" | md5sum | head -c 16)
         if [ $md5here = $md5 ]
         then
-            echo "OK"
+            OK
         else
-            echo "MD5 mismatch"
-            exit 1
+            FAIL
         fi
     done
 }
@@ -30,12 +50,12 @@ quux
 hoge
 piyo
 huga
-hero' | ./para --buffered "$@" ./test/para-cmd.bash '{}' | check_outputs
+hero' | $PARA --buffered "$@" ./test/para-cmd.bash '{}' | check_outputs
 
 
 
 LOG "Buffered output test for --map"
-./para --buffered --map "$@" ./test/para-cmd.bash one two three four five six seven eight nine ten | check_outputs
+$PARA --buffered --map "$@" ./test/para-cmd.bash one two three four five six seven eight nine ten | check_outputs
 
 
 testout="./.${0##*/}.$$"
@@ -48,14 +68,41 @@ sleep 1; false
 sleep 1; true
 sleep 1; false
 sleep 1; true
-sleep 1; false' | ./para sh -c '{}' 2> "$testout"; ret=$?
+sleep 1; false' | $PARA sh -c '{}' 2> "$testout"; ret=$?
 
 if [ "$(<$testout)" = 'WARN: para: Had 4 errors.' -a $ret -eq 1 ]; then
-    echo "OK"
+    OK
 else
     echo "FAILED: unexpected (or lack of) error message and/or wrong exit code"
     echo "warn='$warn'"
     echo "exit code=$ret"
-    exit 1
+    FAIL
 fi
 rm -f "$testout"
+
+
+full='/hoge/piyo/huga.ext'
+noext='/hoge/piyo/huga'
+base='huga.ext'
+dirname='/hoge/piyo'
+base_noext='huga'
+
+
+LOG "Checking {} substitution"
+ASSERT_EQ $(echo "$full" | $PARA echo '@{}@{}@') "@$full@$full@"
+
+LOG "Checking {.} substitution"
+ASSERT_EQ $(echo "$full" | $PARA echo '@{.}@{.}@') "@$noext@$noext@"
+
+LOG "Checking {/} substitution"
+ASSERT_EQ $(echo "$full" | $PARA echo '@{/}@{/}@') "@$base@$base@"
+
+LOG "Checking {//} substitution"
+ASSERT_EQ $(echo "$full" | $PARA echo '@{//}@{//}@') "@$dirname@$dirname@"
+
+LOG "Checking {/.} substitution"
+ASSERT_EQ $(echo "$full" | $PARA echo '@{/.}@{/.}@') "@$base_noext@$base_noext@"
+
+LOG "Checking ALL of {*} substitution"
+ASSERT_EQ $(echo "$full" | $PARA echo '@{}@{.}@{/}@{//}@{/.}@') "@$full@$noext@$base@$dirname@$base_noext@"
+
